e2b140005c4b8a2a8472fc5bc062bd628e9117dc
Harry Moore <hjmoore420@gmail.com>
2020-08-13 13:28:55 -0400

    update to navbar

--- component/navBar.js
+++ component/navBar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home
+                <a className="nav-link" href="/">Home
                   <span className="sr-only">(current)</span>
                 </a>
               </li>
@@ -18,10 +18,10 @@ const Navbar = () => {
                 <a className="nav-link" href="about">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
+                <a className="nav-link" href="services">Services</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link" href="contact">Contact</a>
               </li>
             </ul>
           </div>
